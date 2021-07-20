d7f153645f3901c24e728e0a87edc8c577e83ee0
Bastien Daugenne <bastien.daugenne@gmail.com>
2021-07-20 12:28:24 +0200

    Corrections Exercices Part 2 Javascript

--- Part2_exo5_JS.js
+++ Part2_exo5_JS.js
@@ -1,9 +1,19 @@
 // Ex5 Part 2 : Fonction String et paragraphe
-let string = "Reprehenderit commodo amet dolore tempor velit reprehenderit qui ut.";
+// let string = "Reprehenderit commodo amet dolore tempor velit reprehenderit qui ut.";
 
-function nombreCarac() {
-    document.getElementById("paragraphe");
-    document.write("Longueur de la variable string: " + string.length)
+// function nombreCarac() {
+//     document.getElementById("paragraphe");
+//     document.write("Longueur de la variable string: " + string.length)
+// }
+
+// nombreCarac();
+
+//Créer un programme qui permet de compter le nombre de caractères de votre choix
+
+let str1 = "Reprehenderit commodo amet dolore tempor velit reprehenderit qui ut.";
+function strCompt(cara) {
+  let result = cara.length;
+  document.getElementById("paragraphe").innerHTML = result;
+  return result;
 }
-
-nombreCarac();
+console.log(strCompt(str1));
--- Part2_exo6_JS.js
+++ Part2_exo6_JS.js
@@ -1,20 +1,30 @@
 // Ex6 Part 2 : Bienvenue à Bora Bora
 
-let titreCap = "Bienvenue à Bora Bora";
-let paraMinus = "MINIM TEMPOR NOSTRUD VOLUPTATE ALIQUIP VOLUPTATE LABORUM TEMPOR EX EX.";
-let paraCoul = "Sit elit ad enim do reprehenderit aliqua.";
-let paraMasque = "Ut anim do laboris dolor duis deserunt aute proident quis consequat aliquip culpa cupidatat.";
+// let titreCap = "Bienvenue à Bora Bora";
+// let paraMinus = "MINIM TEMPOR NOSTRUD VOLUPTATE ALIQUIP VOLUPTATE LABORUM TEMPOR EX EX.";
+// let paraCoul = "Sit elit ad enim do reprehenderit aliqua.";
+// let paraMasque = "Ut anim do laboris dolor duis deserunt aute proident quis consequat aliquip culpa cupidatat.";
 
-function styleText() {
-    document.getElementById("titre")
-    document.getElementById("p1")
-    document.getElementById("p2")
-    document.getElementById("p3")
+// function styleText() {
+//     document.getElementById("titre")
+//     document.getElementById("p1")
+//     document.getElementById("p2")
+//     document.getElementById("p3")
 
-    document.write(titreCap.toUpperCase()+"<br>")
-    document.write(paraMinus.toLowerCase()+"<br>")
-    document.write(paraCoul.fontcolor("red")+"<br>")
-    document.write(paraMasque.hide())
-}
+//     document.write(titreCap.toUpperCase()+"<br>")
+//     document.write(paraMinus.toLowerCase()+"<br>")
+//     document.write(paraCoul.fontcolor("red")+"<br>")
+//     document.write(paraMasque.hide())
+// }
 
-styleText();
+// styleText();
+
+let H1 = document.getElementById("titre");
+let P1 = document.getElementById("p1");
+let P2 = document.getElementById("p2");
+let P3 = document.getElementById("p3");
+
+H1.innerHTML=H1.innerHTML.toUpperCase();
+P1.innerHTML=P1.innerHTML.toLowerCase();
+P2.style.color="red";
+P3.style.display="none";
